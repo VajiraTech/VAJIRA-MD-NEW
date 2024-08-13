5fa043db98bcfb5157ee5c77178e8590698d13f9
Darksadas <168348276+Darksadas@users.noreply.github.com>
2024-08-13 17:43:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=7R83WC6I#Qjfe7e_7EZVS3QEseXhkPJJraZdQEojKGpCszeNdGlY' ,
+SESSION_ID: process.env.SESSION_ID || ' ' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
