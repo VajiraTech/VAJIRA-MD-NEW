edd06900f73cd6efbffa704a7986e8d18899958b
Nonamess1 <Dctrixx@gmail.com>
2024-08-24 22:20:48 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('s');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
