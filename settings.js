677ca02834e981d1f4fbb45ab21cd9e2bdb2a2a5
Zubairahmed-cloud <mianzubii7096660@gmail.com>
2024-08-24 14:59:21 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || ' ' ,
+VAJIRA-MD=7ZFz2AxS#PJMiNzBVVQWhOjIUayiW4u6RYidW1Ur9AnJMGrVuW1Y ' ' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
