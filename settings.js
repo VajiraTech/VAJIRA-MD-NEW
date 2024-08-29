919197b77db8672643ca3cf825a6816f7f253516
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-08-29 21:59:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=nVFUVJpS#uGdm8tMPJKQ1ZZHXVLA21vScd0cZyPs6ATOR4byGfhU' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=TYtimbIb#HY_SIfF-DLgNXIbKs4uqrmBNHsDgau04rla1HJjsnSU' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
