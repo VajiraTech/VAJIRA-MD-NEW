a6a6fefe30b38a6391c88d6d25ff0f8fe67c4eca
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-08-30 09:44:11 +0530

    👨‍💻 ᴠᴀᴊɪʀᴀ ᴍᴅ ᴠ6 ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻

--- settings.js
+++ settings.js
@@ -4,7 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
-
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
