23e7e25fc0cde488c09d1d50d5da9064c01f8a22
Mr-509 <claudinepierremarie92@gmail.com>
2024-08-31 12:29:17 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
-Ioconst fs = require('fs');
+UnIoconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? ''VAJIRA-MD=6QlBlBzS#tgQQhosKcGoiDnaneydEzkEWCC7_jev_4MNvC_NxjlM : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? ''+50943504463 : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=6QlBlBzS#tgQQhosKcGoiDnaneydEzkEWCC7_jev_4MNvC_NxjlM' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '50943504463' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
