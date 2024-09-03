77d20a476670486be0da40dd1abf6824c26ad272
Munibrustam <107187627+Munibrustam@users.noreply.github.com>
2024-09-03 20:51:25 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=CFtmibaT#_g-NWlSEm
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '923082741184' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
-AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
+AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || true  ,    
 PREFIX: process.env.PREFIX || '.' ,
 FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 Mr Kharus 🤓 👨‍💻': process.env.FOOTER,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
