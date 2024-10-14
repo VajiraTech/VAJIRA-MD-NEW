b5e093da5d1c61862aa79136f6d1fd7c6229e7e1
Rashika3 <rashikasadaruwan94@gmail.com>
2024-10-14 08:19:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.VAJIRA-MD=jVN2AZrR#iL1bpScb6rrL34WeVPUvN4a6rdoLkowboUDpLHEBK4U,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.94726402862,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
