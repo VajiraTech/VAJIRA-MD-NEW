cfd939dc70e3ab9de84eba568d5a727d9b9b6910
TanyaMashamba01 <mildredtanyamashamba01@gmail.com>
2024-10-18 06:41:05 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=fdNylASQ#_ZvKW0Dntuf0191TnTH7ZfXwGQVXJlSmozF0CWuisNg' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+263777756184' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=PYtDjaRa#nv8y7nHFKTFTPahL80XKIjo9Up6mpiNzfHliKrm-u-w' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+263716729222' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || true  ,    
